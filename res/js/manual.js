be32ad245ed6a06e6082056e907ca8a05d8cdab9
JakobTischler <jakob.tischler@gmail.com>
2014-12-18 17:32:46 +0100

    add nav menu to other pages

--- res/js/manual.js
+++ res/js/manual.js
@@ -1,29 +1,16 @@
 ;(function ($) { $(document).ready(function () {
 $('body').removeClass('no-js').addClass('js');
 
-
-
-/**
-function startsWith(str, startStr) {
-	return str.substr(0, startStr.length) == startStr;
-}
-
-var currentId = '#intro';
+// if hash isn't used in url, default to "#intro"
 var hash = window.location.hash;
-if (hash && hash !== 'undefined') {
-	currentId = hash.substr(1, hash.length - 1); // omit the hash sign
-	// currentId = hash;
+if (!hash || hash == '' || hash == 'undefined') {
+	if (history.pushState) {
+		history.pushState(null, null, '#intro');
+		// console.log('pushState "#intro"')
+	} else {
+		location.hash = '#intro';
+		// console.log('set location.hash to "#intro"')
+	}
 };
-// console.log(currentId, hash)
-
-if (startsWith(currentId, 'hud')) {
-	console.log('startsWith hud')
-	// TODO
-} else if (startsWith(currentId, 'driveMode')) {
-	console.log('startsWith driveMode')
-	// TODO
-};
-**/
-
 
 }); })(jQuery); //END document.ready
